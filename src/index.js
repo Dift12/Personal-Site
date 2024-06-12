88f5544c461aa189e7efeba2c6ef1e7a187bcc5d
dift12 <diftbanda@gmail.com>
2024-06-12 18:44:04 -0300

    Create home as a test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./global-css/global.css";
+
+
+import Home from "./pages/home/home";
+
+
+
+const homeValues={
+    h1: "whoami",
+    listItems: ["work", "stack", "contact", "rm -rf"],
+}
+
+
+const main=document.querySelector("#content");
+
+main.appendChild(Home(homeValues));
+
